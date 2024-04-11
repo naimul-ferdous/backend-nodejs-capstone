50136b8a03bb31015f710c6f10702cae04b34448
Naimul Ferdous <naimul-ferdous@users.noreply.github.com>
2024-04-11 17:35:40 +0600

    Update app.js
    unused imports commented out

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const pinoLogger = require('./logger')
 
 const connectToDatabase = require('./models/db')
-const { loadData } = require('./util/import-mongo/index')
+// const { loadData } = require("./util/import-mongo/index");
 
 const app = express()
 app.use('*', cors())
